dd8b5277593228993c1cbc02cf1dc2aac4ce9971
Gonzalo Martin <gonzalomartin@MacBook-Pro-de-Gonzalo.local>
2024-09-16 19:29:17 -0300

    esqueleto secciones app

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+@app.route('/register')
+def register():  # put application's code here
+    return render_template('register.html')
+
+@app.route('/match-applicants')
+def match_applicants():  # put application's code here
+    return render_template('match-applicants.html')
 
 @app.route('/')
 def index():  # put application's code here
